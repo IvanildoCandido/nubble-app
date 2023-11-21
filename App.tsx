c2cea514b9b2141c8e41226c2894f69ac05a8273
Ivanildo Cândido Bezerra <systechtecnologia@gmail.com>
2023-11-21 19:18:43 -0300

    text 2 - custom fonts

--- App.tsx
+++ App.tsx
@@ -5,7 +5,9 @@ import {Text} from './src/components/Text/Text';
 function App(): JSX.Element {
   return (
     <SafeAreaView>
-      <Text preset="headingLarge">Hello World</Text>
+      <Text preset="headingLarge" style={{fontFamily: 'Pencerio-Hairline'}}>
+        Hello World
+      </Text>
     </SafeAreaView>
   );
 }
